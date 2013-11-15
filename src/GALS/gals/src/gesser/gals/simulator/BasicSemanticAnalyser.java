9c9a6d5a23f1e8df678d332becc112e82c2634cc
Octávio <octaviorichter@gmail.com>
2013-11-14 23:31:50 -0200

    remove fonte do gals

--- src/GALS/gals/src/gesser/gals/simulator/BasicSemanticAnalyser.java
+++
@@ -1,9 +0,0 @@
-package gesser.gals.simulator;
-
-import gesser.gals.analyser.SemanticError;
-import gesser.gals.analyser.Token;
-
-public interface BasicSemanticAnalyser
-{
-	void executeAction(int action, Token currentToken) throws SemanticError;
-}
