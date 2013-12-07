4bb34e3fa6553438527273907bd21c6806b833b0
henriqued <henriquedsg89@gmail.com>
2013-12-07 18:32:24 -0200

    SimbolTest par ao bit fazer

---
+++ test/lexico/SimbolTest.scala
@@ -0,0 +1,13 @@
+package lexico
+
+import controller.Controller
+import gals.Constants
+import org.scalatest._
+/**
+ * User: henrique & octavio
+ * Date: 07/12/13
+ * Time: 18:26
+ */
+class SimbolTest extends FlatSpec with Matchers {
+
+}
