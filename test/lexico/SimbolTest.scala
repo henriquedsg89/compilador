0f02173040e16e6b3879efbf883904f1a04a3515
Octávio <octaviorichter@gmail.com>
2013-12-07 19:50:25 -0200

    teste para simbolos simples e complexos

--- test/lexico/SimbolTest.scala
+++ test/lexico/SimbolTest.scala
@@ -9,5 +9,36 @@ import org.scalatest._
  * Time: 18:26
  */
 class SimbolTest extends FlatSpec with Matchers {
+  val lexico = new Controller().lexico
+  val simbolos = Map(
+    6 -> "+",
+    7 -> "-",
+    8 -> "*",
+    9 -> "/",
+    10 -> "(",
+    11 -> ")",
+    12 -> ";",
+    13 -> ":",
+    14 -> ".",
+    15 -> "=",
+    16 -> ",",
+    17 -> "{",
+    18 -> "}",
+    19 -> "[",
+    20 -> "]",
+    21 -> "..",
+    22 -> ":=",
+    23 -> "<",
+    24 -> ">",
+    25 -> "<>",
+    26 -> "<=",
+    27 -> ">=")
 
+  "Dado todos os simbolos simples e complexos validos" should "validar cada simbolo com seu token id" in {
+    simbolos.values map { value =>
+      lexico.setInput(value)
+      val token = lexico.nextToken()
+      simbolos.get(token.getId).get should be (value)
+    }
+  }
 }
