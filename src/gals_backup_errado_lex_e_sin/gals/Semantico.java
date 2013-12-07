8cd1e879c0e770c86f1c4109e19be0bcc483ab62
Octávio <octaviorichter@gmail.com>
2013-12-07 17:19:52 -0200

    limpa builds e arquivos antigos do projeto

--- src/gals_backup_errado_lex_e_sin/gals/Semantico.java
+++
@@ -1,9 +0,0 @@
-package gals_backup_errado_lex_e_sin.gals;
-
-public class Semantico implements Constants
-{
-    public void executeAction(int action, Token token)	throws SemanticError
-    {
-        System.out.println("Ação #"+action+", Token: "+token);
-    }	
-}
