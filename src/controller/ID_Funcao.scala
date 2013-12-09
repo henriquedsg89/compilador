68508be9bee662dfb1e41bb26e6e5ed413b10a91
henriqued <henriquedsg89@gmail.com>
2013-12-08 23:17:39 -0200

    fix da 24

--- src/controller/ID_Funcao.scala
+++ src/controller/ID_Funcao.scala
@@ -7,4 +7,12 @@ package controller
  */
 case class ID_Funcao(nome: String, nivel: Int, end_prim_instr: Int, num_parms: Int, list_params: Array[ID_Parametro], tipo_resultado: String)
   extends ID_Abstract(nome, 5, nivel, 0) {
+
+  def temPar(id: String) : Boolean = {
+    list_params map { p =>
+      if (p.nome == id)
+        true
+    }
+    false
+  }
 }
